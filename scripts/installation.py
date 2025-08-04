83bae2e507f84e027e867bfc6c7c5f0537a084b5
PILIHU2022 <Spark-CN@outlook.com>
2025-08-04 12:13:31 +0800

    feat(installation): finished the basic installation script some codes should improve

--- scripts/installation.py
+++ scripts/installation.py
@@ -1,11 +1,17 @@
+# WARN: Some codes in this script SHOULD be improved!!!
 import os
+import json
+import time
 
 
 class Arch_installer:
-    def __init__(self, optional_mount_partition) -> None:
-        # self.mirror = mirror
-        self.system_packages = []
-        self.optional_mount_partition = optional_mount_partition
+    def __init__(self) -> None:
+        self.system_packages = [
+            "base",
+            "base-devel",
+            "linux-firmware",
+            "networkmanager",
+        ]
         pass
 
     def ask_partition_name(self, boot_partition, root_partition, swap):
@@ -53,10 +59,16 @@ class Arch_installer:
                     f"""mount -t btrfs -o {arguments} {self.root_partition} /mnt"""
                 )
             # judge user if set subvolume name
-            confirm_subvolume_name = input("Do you want to set your subvolume name? y/n ")
-            if confirm_subvolume_name == 'y':
-                subvolume_name_for_root = input("Input your subvolume name for *root* partition: ")
-                subvolume_name_for_home = input("Input your subvolume name for *home* partition: ")
+            confirm_subvolume_name = input(
+                "Do you want to set your subvolume name? y/n "
+            )
+            if confirm_subvolume_name == "y":
+                subvolume_name_for_root = input(
+                    "Input your subvolume name for *root* partition: "
+                )
+                subvolume_name_for_home = input(
+                    "Input your subvolume name for *home* partition: "
+                )
                 os.system(f"btrfs subvolume create /mnt/{subvolume_name_for_root}")
                 os.system(f"btrfs subvolume create /mnt/{subvolume_name_for_home}")
             else:
@@ -66,17 +78,31 @@ class Arch_installer:
         else:
             pass
 
-    def mount_partition(self):
+    def mount_system_partition(self):
         # judge user if pass argument
         confirm_argument = input("Do you want to pass your own argument? y/n ")
         if confirm_argument == "y":
             user_argument_for_root = input("Input your argument for root: ")
-            user_argument_for_home = input("Input your argument for home(subvol=/<home_subvolume_name>): ")
-            if self.filesysyem_type 
+            user_argument_for_home = input(
+                "Input your argument for home(subvol=/<home_subvolume_name>...): "
+            )
             # mount root
-            os.system(f"mount -t btrfs -o {user_argument_for_root} {self.root_partition}
+            os.system(
+                f"mount -t {self.filesystem_type} -o {user_argument_for_root} {self.root_partition} /mnt"
+            )
             # mount home
-            os.system(f"mount -t btrfs -o {user_argument_for_home} {self.root_partition}
+            os.system("mkdir /mnt/home")
+            os.system(
+                f"mount -t {self.filesystem_type} -o {user_argument_for_home} {self.root_partition} /mnt/home"
+            )
+        else:
+            os.system(f"mount {self.root_partition} /mnt")
+            os.system(f"mount {self.root_partition} /mnt/home")
+        os.system("mkdir /mnt/boot")
+        os.system(f"""mount {self.boot_partition} /mnt/boot""")
+        os.system(f"""swapon {self.swap}""")
+        time.sleep(5)
+        os.system("clear")
 
     def change_mirror(self):
         mirror_list = {
@@ -109,17 +135,70 @@ class Arch_installer:
         os.system("cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.back")
         with open("/etc/pacman.d/mirrorlist", "w") as mirror_file:
             mirror_file.write(mirror)
+        time.sleep(5)
         os.system("clear")
 
+    def ask_packages_name_for_install(self):
+        # For kernel
+        print(
+            "Please visit this page to choose a kernel: https://wiki.archlinuxcn.org/wiki/Kernel"
+        )
+        kernel_name = input(
+            "Input your kernel package name to install(ONLY official repo supported!!!), input should be `linux(-...)`: "
+        )
+        self.system_packages.append(kernel_name)
+        while True:
+            packages_user_install = input(
+                "Input package name you want to install(in official repo, no in AUR, input `q` to exit): "
+            )
+            if packages_user_install == "q":
+                break
+            else:
+                self.system_packages.append(packages_user_install)
 
-installer = Arch_installer(
-    # input("partitions"),
-    # input("system_packages"),
-    input("optional_mount_partition"),
-)
+    def install_packages(self):
+        for item in self.system_packages:
+            os.system(f"pacstrap /mnt {item}")
+
+    def genarate_fstab(self):
+        os.system("genfstab -U /mnt > /mnt/etc/fstab")
+
+
+def optional_mount_partition(willing):
+    if willing == "y":
+        os.system("lsblk -f")
+        print(
+            "Please input the partitions you want to mount(this will be mounted without arguments)!!!"
+        )
+        while True:
+            to_be_mounted_partition = input(
+                "Input(/dev/.... If you WANT to exit, please press`q` and return):"
+            )
+            if to_be_mounted_partition != "q":
+                os.system(f"mount {to_be_mounted_partition}")
+            elif to_be_mounted_partition == "q":
+                break
+    elif willing == "n":
+        return
+    else:
+        print("Please input y/n")
+
+
+def chroot():
+    os.system("arch-chroot /mnt")
+
+
+installer = Arch_installer()
 installer.ask_partition_name(
     boot_partition=input("Input your boot partition:"),
     root_partition=input("Input your root partition:"),
     swap=input("Input your swap partition:"),
 )
+# TODO
 installer.change_mirror()
+installer.ask_packages_name_for_install()
+installer.install_packages()
+optional_mount_partition(input("Do you want to mount other partitions? y/n: "))
+chroot()
+# NOTE: This wanted to store user's choses and print it let them confirm
+# json.dump()
